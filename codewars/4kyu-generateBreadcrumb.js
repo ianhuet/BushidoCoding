ef9419945798d07c9430802269eeb68df907e299
Ian Huet <ian.huet@eachandother.com>
2017-10-02 13:54:32 +0100

    4kyu-GenerateBreadcrumb completed

--- codewars/4kyu-generateBreadcrumb.js
+++ codewars/4kyu-generateBreadcrumb.js
@@ -1,14 +1,15 @@
 function generateBC(url, separator){
-  console.log(url);
   var crumbs = url.replace(/^(http|https):\/\//, '').replace(/\/index.([^/]*)$/, '').replace(/\/$/, '').split('/');
   var breadcrumbs = '';
-  
-  if(crumbs.length > 1) {
-    
-    var root = crumbs.shift();
+
+  if(crumbs.length <= 1) {
+    breadcrumbs = '<span class="active">HOME</span>';
+  }
+  else {
+    var base = crumbs.shift();
     var current = crumbs.pop();
     var crumbed = crumbs;
-  
+
     var glutenFree = function(str) {
       var exempt = ["the","of","in","from","by","with","and","or","for","to","at","a"];
       return str.split('-').filter(e => !exempt.includes(e)).map(n => n.charAt(0)).join('');
@@ -20,17 +21,15 @@ function generateBC(url, separator){
     };
     var makeBread = crumbed.map(function(c,i){
       c = (c.length > 30) ? glutenFree(c) : c;
-      return '<a href="'+ getPath(crumbs,i+1) + '">' + c.toUpperCase() + '</a>';
+      return '<a href="'+ getPath(crumbs,i+1) + '">' + c.toUpperCase().replace(/-/g,' ') + '</a>';
     });
-    
+
     current = (current.search(/(\.|\?|#)/)>-1) ? current.split(/(\.|\?|#)/)[0] : current;
     current = (current.length > 30) ? glutenFree(current) : current;
 
     breadcrumbs = (makeBread.length > 0) ? separator+ makeBread.join(separator) : '';
-    breadcrumbs = '<a href="/">HOME</a>' +breadcrumbs +separator +'<span class="active">' +current.toUpperCase().replace('-',' ') +'</span>';  
-  } else {
-    breadcrumbs = '<span class="active">HOME</span>';
+    breadcrumbs = '<a href="/">HOME</a>' +breadcrumbs +separator +'<span class="active">' +current.toUpperCase().replace(/-/g,' ') +'</span>';  
   }
-  
+
   return breadcrumbs;
 }
