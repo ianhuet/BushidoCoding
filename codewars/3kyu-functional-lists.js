2233805808d94e7aa6169a885ad4ef0819313cd8
Ian Huet <ian.huet@eachandother.com>
2017-10-05 00:50:42 +0100

    Functional List: add toString method

--- codewars/3kyu-functional-lists.js
+++ codewars/3kyu-functional-lists.js
@@ -1,8 +1,8 @@
-function LinkedList(){  
+function LinkedList(){
   this.head = null;
 }
 LinkedList.prototype.push = function(val){
-  var node = {
+  let node = {
     value: val,
     next: null
   }
@@ -17,19 +17,47 @@ LinkedList.prototype.push = function(val){
   }
   return this;
 }
+// LinkedList.prototype.insertAfter = function(val) {
+//   let node = {
+//     value: val,
+//     next: null
+//   }
+//   if(! this.head){
+//     this.head = node;
+//   } else {
+//     current = this.head;
+//     while(current.next){
+//       current = current.next;
+//     }
+//     current.next = node;
+//   }
+//   return this;
+// }
 LinkedList.prototype.isEmpty = function(){
   return this._length;
 }
 LinkedList.prototype.toString = function(){
-  return this._length;
+  let str = '(';
+  if(this.head){
+    current = this.head;
+    while(current.next){
+      str += current.value;
+      current = current.next;
+    }
+  }
+  return str + ')';
 }
 
 
 var mt, l1, l2, l3, l4;
 mt = new LinkedList();
+console.log(mt.toString());
 l1 = mt.push('c').push('b').push('a');
 console.log(l1);
+console.log(l1.toString());
 
 // l2 = l1.insertAfter(l1);
+// console.log(l2);
+
 // l3 = l1.remove('b');
 // l4 = l2.remove('b');
