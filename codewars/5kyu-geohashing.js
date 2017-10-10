e6f2bad47f45f1327c4319475b79965e81362f8a
Ian Huet <ian.huet@eachandother.com>
2017-10-10 17:07:31 +0100

    5kyu-closestFriends - all test passed

---
+++ codewars/5kyu-geohashing.js
@@ -0,0 +1,17 @@
+function geoHashing(dow, date) {
+  console.log(dow);
+  console.log(date);
+
+  let date = new Date(date);
+  console.log(date);
+
+
+}
+
+
+
+
+let dow1  = 10458.68;
+let date1 = 2005-05-26T00:00:00.000Z
+
+console.log(geoHashing(dow1,date1));
