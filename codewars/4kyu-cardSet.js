369ce4bee26f81c153cf49c94426bfccb75bd1ec
Ian Huet <ian.huet@eachandother.com>
2017-11-21 20:49:15 +0000

    Last update of 4kyu-CardSet before submitFinal

--- codewars/4kyu-cardSet.js
+++ codewars/4kyu-cardSet.js
@@ -1,55 +1,19 @@
 'use strict';
 
-// let cards = [
-//   [[3,1,1,1],[3,1,1,2],[3,1,1,3]],
-//   [[2,2,1,1],[1,2,2,2],[1,2,2,3]],
-//   [[1,1,1,1],[1,2,1,2],[1,1,1,3]],
-//   [[2,1,1,1],[2,2,2,2],[2,1,1,3]]
-// ];
-
-
-/*
-let cards = [
-  [[1,1,1,1],[1,2,1,2],[1,1,1,3]],
-  [[2,1,1,1],[2,2,2,2],[2,1,1,3]],
-  [[3,1,1,1],[3,1,1,2],[3,1,1,3]],
-  [[2,2,1,1],[1,2,2,2],[1,2,2,3]]
-];
-
-// Expected: 2, instead got: 5
-// [[0,0],[1,0],[2,0]]
-// [[0,0],[1,2],[2,1]]
-// [[0,2],[1,0],[2,1]]
-// [[0,2],[1,2],[2,2]]
-// [[2,0],[2,1],[2,2]]
-*/
-
-
-let cards = [
-  [[2,2,2,2],[2,1,1,3],[2,1,1,1]],
-  [[3,1,1,2],[3,1,1,3],[3,1,1,1]],
-  [[1,2,2,2],[1,2,2,3],[2,2,1,1]],
-  [[1,1,1,1],[1,2,1,2],[1,1,1,3]]
-];
-
-// Expected: 2, instead got: 1
-// [[[0,1],[1,0],[3,0]]]
-
-
-
 function getSolutions(cards) {
   const flattenDeck = (c) => c.reduce((a,b) => a.concat(b));
   const pruneSets   = (s) => Array.from(new Set(s));
+  const convertStrToSet = (s) => {
+    s = s.map(s => s.split('-')).map(n => new Set(n));
+    return s;
+  };
 
-  // const convertTo2D = (s) => s.map((n) => deckPos(line[parseInt(n)]));
-  function convertTo2D(s) {
-    return s.map(function(n){
+  const convertTo2D = (s) => {
+    return s.map((n) => {
       n = Array.from(n);
-      return n.map(function(m){
-        return deckPos(line[m]);
-      });
+      return n.map((m) => deckPos(line[m]));
     });
-  }
+  };
 
   const deckPos = (card) => {
     for(var x=0; x<cards.length; x++){
@@ -63,7 +27,21 @@ function getSolutions(cards) {
     if ((c1[t] === c2[t]) && (c2[t] === c3[t]) && (c1[t] === c3[t])) return true
     if ((c1[t] !== c2[t]) && (c2[t] !== c3[t]) && (c1[t] !== c3[t])) return true;
     return false;
-  }
+  };
+
+  const findOverlap = (s1,s2) => {
+    let overlap = new Set();
+    s1.forEach(e => { if(s2.has(e)) overlap.add(e) });
+    return overlap.size;
+  };
+
+  const inSet = (ss,s) => {
+    let filtered = ss.filter(function(n){
+      return findOverlap(n,s);
+    });
+    return filtered.length > 0;
+  };
+
 
   const findAllSets = () => {
     let cardL=12, set;
@@ -81,37 +59,27 @@ function getSolutions(cards) {
               set = [x,y,z].sort((a,b) => a-b).join('-');
               sets.push(set);
     } } } } }
-  }
+  };
 
-  function countMultiUse(s) {
-    let count={};
-    s.forEach(s=>count[s]=(count[s]||0)+1);
-    return count;
-  }
+  const findValidSets = (s) => {
+    let s0=[], i=0, first=true;
 
-  function getMostUsed(c) {
-    return Object.keys(c).reduce((a,b) => c[a] > c[b] ? a : b);
-  }
+    s.forEach(function(n){
+      for(let x=i; x<s.length; x++) {
+        if ((x!==i) && !findOverlap(s[i],s[x]) && ! inSet(s0,s[x])) {
 
-  function findOverlap(s1,s2) {
-    let overlap = new Set();
-    s1.forEach(e => { if(s2.has(e)) overlap.add(e) });
-    return overlap.size;
-  }
+          if(first) {
+            s0.push(s[i]);
+            first = false;
+          }
 
-  function removeMultiUse(s) {
-    let s0=[], i;
-
-    s = s.map(s => s.split('-')).map(n => new Set(n));
-    s0.push(s[0]);
-
-    i = s.length-1;
-    while (i-- && i>0) {
-      if(! findOverlap(s[0],s[i])) s0.push(s[i]);
-    }
+          s0.push(s[x]);
+        }
+      } 
+      i++;
+    });
     return s0;
-  }
-
+  };
 
 
   let sets=[];
@@ -119,11 +87,10 @@ function getSolutions(cards) {
 
   findAllSets();
   sets = pruneSets(sets);
-  sets = removeMultiUse(sets);
+  sets = convertStrToSet(sets);
+  sets = (sets.length > 1) ? findValidSets(sets) : sets;
   sets = convertTo2D(sets);
 
-  console.log(cards);
-  console.log(sets);
   return sets;
 }
 
