2c5d69e1db22fd59f294aee9ba55268bf56453b5
Ian Huet <ian@huet.info>
2018-01-22 19:38:56 +0000

    3kyu-gpsNavigation complete

--- codewars/3kyu-gpsNavigation.js
+++ codewars/3kyu-gpsNavigation.js
@@ -1,39 +1,57 @@
 'use strict'
 
 function navigate(numberOfIntersections, roads, start, finish) {
-
-    let node=start, path=null;
-    let options=[], route=[], processed=[];
-
-    route.push(node);
-
-    // find all paths starting with node
-    while (node !== finish) {
-
-        // find eligible paths
-        options = roads.filter(p => p.from == node);
-
-        // find lowest path time
-        path = options.reduce((lowest, p) => {
-            if(p.to === finish || lowest === null || p.drivingTime < lowest.drivingTime) {
-                if (! processed.includes(node)) {
-                    lowest = node;
+    
+    const lowestCostNode = (costs, processed) => {
+        return costs.reduce((lowest, costVal, i) => {
+            if (lowest === null || costs[i] < costs[lowest]) {
+                if (! processed.includes(i)) {
+                    lowest = i;
                 }
             }
             return lowest;
         }, null);
+    };
+    const getPathOptions = (n) => roads.filter(path => path.from == n);
 
-        // track node as processed
-        processed.push(node);
+    let costs=[], parents=[], processed=[];
+    let currentNode = start;
+    parents[start] = 0;
+    costs[start] = 0;
 
-        // add path to route
-        console.log(node, path);
+    while (currentNode != null && currentNode != finish) {
+        let options = getPathOptions(currentNode);
 
-        node = path.to;
-        route.push(node);
+        options.forEach(opt => {
+            let newCost = costs[currentNode] + opt.drivingTime;
+
+            if (! processed.includes(opt.to) && ! costs[opt.to]) {
+                costs[opt.to] = newCost;
+                parents[opt.to] = currentNode;
+            }
+
+            if (! processed.includes(opt.to) && costs[opt.to] > newCost) {
+                costs[opt.to] = newCost;
+                parents[opt.to] = currentNode;
+            }
+        });
+
+        processed.push(currentNode);
+        currentNode = lowestCostNode(costs, processed);
     }
 
-    return route;
+    if(currentNode == null) {
+        return currentNode;
+    }
+    else {
+        let route=[], i=finish;
+        while(i != start) {
+            route.push(i);
+            i = parents[i];
+        }
+        route.push(i); route.reverse();
+        return route;    
+    }
 }
 
 var roads = [
@@ -46,4 +64,24 @@ var roads = [
     {from: 3, to: 2, drivingTime: 2},
     {from: 3, to: 4, drivingTime: 10}
 ];
-console.log(navigate(5, roads, 0, 4));
+// var roads = [
+//     { from: 0, to: 1, drivingTime: 7 },
+//     { from: 0, to: 2, drivingTime: 9 },
+//     { from: 0, to: 5, drivingTime: 14 },
+//     { from: 2, to: 5, drivingTime: 2 },
+//     { from: 2, to: 3, drivingTime: 11 },
+//     { from: 5, to: 0, drivingTime: 14 },
+//     { from: 5, to: 2, drivingTime: 2 },
+//     { from: 5, to: 4, drivingTime: 9 },
+//     { from: 4, to: 5, drivingTime: 9 },
+//     { from: 4, to: 3, drivingTime: 6 },
+//     { from: 3, to: 4, drivingTime: 6 },
+//     { from: 3, to: 2, drivingTime: 11 },
+//     { from: 3, to: 1, drivingTime: 15 },
+//     { from: 1, to: 0, drivingTime: 7 },
+//     { from: 1, to: 3, drivingTime: 15 },
+//     { from: 1, to: 2, drivingTime: 10 },
+//     { from: 2, to: 1, drivingTime: 10 },
+//     { from: 2, to: 0, drivingTime: 9 }
+// ];
+console.log(navigate(6, roads, 0, 5));
